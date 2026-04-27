455bb2bd938dd77631b7e5fd2864be91beaaf9ef
Thiago Klein <87211953+ThiagoKleinn@users.noreply.github.com>
2026-04-27 17:35:02 -0300

    Queue (Fila Encadeada)

---
+++ fila2.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Elem {
+    int valor;
+    struct Elem* prox;
+} Elem;
+
+typedef struct {
+    Elem* inicio;
+    Elem* fim;
+} Fila;
+
+void inicializarFila(Fila* f) {
+    f->inicio = NULL;
+    f->fim = NULL;
+}
+
+void enfileirar(Fila* f, int valor) {
+    Elem* novo = (Elem*)malloc(sizeof(Elem));
+    novo->valor = valor;
+    novo->prox = NULL;
+
+    if (f->fim == NULL) {
+        f->inicio = f->fim = novo;
+    } else {
+        f->fim->prox = novo;
+        f->fim = novo;
+    }
+}
+
+int desenfileirar(Fila* f, int* valorRemovido) {
+    if (f->inicio == NULL)
+        return 0;
+
+    Elem* temp = f->inicio;
+    *valorRemovido = temp->valor;
+    f->inicio = temp->prox;
+
+    if (f->inicio == NULL)
+        f->fim = NULL;
+
+    free(temp);
+    return 1;
+}
+
+int contem(Fila* f, int valor) {
+    Elem* atual = f->inicio;
+    while (atual != NULL) {
+        if (atual->valor == valor)
+            return 1;
+        atual = atual->prox;
+    }
+    return 0;
+}
+
+void uniaoFilas(Fila* f1, Fila* f2) {
+    int valor;
+    while (desenfileirar(f2, &valor)) {
+        if (!contem(f1, valor)) {
+            enfileirar(f1, valor);
+        }
+    }
+}
+
+void imprimirFila(Fila* f) {
+    Elem* atual = f->inicio;
+    while (atual != NULL) {
+        printf("%d ", atual->valor);
+        atual = atual->prox;
+    }
+    printf("\n");
+}
+
+int main() {
+    Fila f1, f2;
+    inicializarFila(&f1);
+    inicializarFila(&f2);
+
+    enfileirar(&f1, 1);
+    enfileirar(&f1, 2);
+    enfileirar(&f1, 3);
+    enfileirar(&f2, 2);
+    enfileirar(&f2, 4);
+    enfileirar(&f2, 5);
+
+    uniaoFilas(&f1, &f2);
+
+    imprimirFila(&f1);
+    imprimirFila(&f2);
+
+    return 0;
+}
